24e8c1a0c44712c3685faffd4597d8767be4d05b
Bannavith Khuhaprema <bannavithkhu@mathregit.group>
2020-07-21 15:13:11 +0700

    add initial routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-<<<<<<< HEAD
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -12,20 +11,6 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule
-=======
-import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { ProductListComponent } from './product-list/product-list.component';
-
-@NgModule({
-  declarations: [
-    AppComponent,
-    LoginComponent,
-    ProductListComponent
-  ],
-  imports: [
-    BrowserModule
->>>>>>> 72472e30a2702a019ad53f6c0a4efa7e7a399334
   ],
   providers: [],
   bootstrap: [AppComponent]
