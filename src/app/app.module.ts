566e14fc1ec66ddb7107775a1764740f69938673
Bannavith Khuhaprema <bannavithkhu@mathregit.group>
2020-07-21 14:41:22 +0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { ProductListComponent } from './product-list/product-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ProductListComponent
   ],
   imports: [
     BrowserModule
