e008917d08cf322638cd1343fd9a92d7342025f6
Bannavith Khuhaprema <bannavithkhu@mathregit.group>
2020-07-22 10:56:34 +0700

    custom pipe feature 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {FormsModule} from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { ProductListComponent } from './product-list/product-list.component';
+import { ProductPipe } from './product.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    ProductListComponent
+    ProductListComponent,
+    ProductPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -8,12 +8,11 @@ import { Product } from './../models/product';
 })
 export class ProductListComponent implements OnInit {
   products: Product[] = [];
+  filterData;
   constructor() {
     const p1 = new Product("001", "soap", 123,"yes");
     const p2 = new Product("002", "soap", 123,"yes");
     const p3 = new Product("003", "soap", 123,"yes");
-    
-
     this.products.push(p1, p2, p3);
   }
 
